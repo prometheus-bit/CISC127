a3ab57f6b388f96cd24268e9972f498e303fffd2
prometheus-bit <Matthew.prussman46@myhunter.cuny.edu>
2017-09-08 19:05:36 -0400

    INitial Commit of Previous projects

--- Ceasat.py
+++ Ceasat.py
@@ -1,3 +1,42 @@
 # Name: Matthew Prussman
 # Date: September 1, 2017
 # This program prints: ASCII code of characters from STDIN to STDOUT
+
+import sys
+
+def byteReadLine():
+
+    try:
+        return sys.stdin.buffer.readline()
+    except:
+        print("read error")
+
+
+
+def bitLessOne(bit):
+
+    shifter = 1
+    bit ^= shifter
+
+    while bit > bit ^ shifter:
+        shifter <<= 1
+        bit ^= shifter
+
+    return bit
+
+def cipherBytes(bytePhrase):
+
+    if bytePhrase == 97:
+        yield 122
+    else:
+        yield bitLessOne(bytePhrase)
+
+def prettyPrintCipher(gen):
+
+    for byte in gen:
+        for ciphered in cipherBytes(byte):
+            yield f'{ciphered:c}'
+
+if __name__ == '__main__':
+    print("Please enter phrase")
+    print("Your word in code is:\n", *prettyPrintCipher(byteReadLine()), sep='')
